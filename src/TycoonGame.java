751e0b6bfd5abfd1eacd71f6121e3b54e6551fe0
ginaseo <iamginaseo@gmail.com>
2021-12-17 04:09:49 +0900

    Initial commit

---
+++ src/TycoonGame.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class TycoonGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GamePanel gp = new GamePanel();
+
+        while (true) {
+            System.out.println("=== Bungeoppang Tycoon Game ===");
+            System.out.println("1.StartGame");
+            System.out.println("2.Manual");
+            System.out.println("3.Options");
+            System.out.println("4.Credits");
+            System.out.println("5.ExitGame");
+            System.out.println("===============================");
+            System.out.println("");
+            System.out.print("your input > ");
+            int input = sc.nextInt();
+
+            switch (input) {
+                case 1:
+                    System.out.println("The game starts!");
+                    gp.startGame();
+                    break;
+                case 2:
+                    System.out.println("Manual");
+                    break;
+                case 3:
+                    System.out.println("Options");
+                    break;
+                case 4:
+                    System.out.println("Credits:ginaseo");
+                    break;
+                case 5:
+                    System.out.println("The game will now exit.");
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter a number less than 6");
+            }
+
+
+        }
+
+    }
+
+}
+
